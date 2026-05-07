50aaa23519a2e2f82d2eccc190db5d843c55a775
NathanGeraldino <127454900+NathanGeraldino@users.noreply.github.com>
2026-05-07 13:18:46 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -550,9 +550,9 @@ function renderDashboardLowStock() {
         <tbody>
           ${baixos.map((item) => `
             <tr>
-              <td>${mov.data}</td>
               <td class="produto-nome">${item.nome}</td>
               <td>${item.modelo || "-"}</td>
+              <td>${item.data}</td>
               <td>${item.quantidade}</td>
               <td>${item.minimo}</td>
               <td><span class="status low">Estoque baixo</span></td>
